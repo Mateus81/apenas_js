8aff45f9f3aabcc70ca9001820fdb46751a23dd2
Mateus81 <msnmateus@yahoo.com.br>
2021-11-11 13:56:49 -0200

    adicionando closure

---
+++ functional/closure.js
@@ -0,0 +1,11 @@
+let x = 50;
+
+function fora(){
+    function somarXMais3(){
+        return x + 3;
+    }
+    return somarXMais3
+}
+
+
+module.exports = fora()
---
+++ functional/index_closure.js
@@ -0,0 +1,4 @@
+const somarXMais3 = require('./closure');
+
+let x = 100;
+console.log(somarXMais3())
