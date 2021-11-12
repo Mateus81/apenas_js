b8770c2a2323d2f1e97bce5a8d3836f5b321eb5f
Mateus81 <msnmateus@yahoo.com.br>
2021-11-12 14:10:47 -0200

    adicionando sets

---
+++ sets.js/setj.js
@@ -0,0 +1,15 @@
+let jogos = new Set();
+jogos.add("Subnautica");
+jogos.add("Metal Gear Solid");
+jogos.add("Crosscode");
+jogos.add("Forza Horizon 5")
+jogos.delete("Crosscode")
+
+console.log(jogos)
+console.log(jogos.has("The Witcher 3")); // false
+
+console.log(jogos.size) // 3
+
+var marcas = ["Adidas", "Nike", "Puma"];
+var mySet = new Set(marcas);
+console.log(mySet.has("Adidas"));
